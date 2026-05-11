ef6e264acc7e720f92a367cb44e4b3fa8cf5b4e7
胡官进 <1433375952@qq.com>
2026-05-11 13:23:20 +0800

    修复ip记录不执行bug

--- model/option.go
+++ model/option.go
@@ -225,7 +225,7 @@ func updateOptionMap(key string, value string) (err error) {
 			common.ImageDownloadPermission = intValue
 		}
 	}
-	if strings.HasSuffix(key, "Enabled") || key == "DefaultCollapseSidebar" || key == "DefaultUseAutoGroup" {
+	if strings.HasSuffix(key, "Enabled") || key == "DefaultCollapseSidebar" || key == "DefaultUseAutoGroup" || key == "RecordErrorIpLog" {
 		boolValue := value == "true"
 		switch key {
 		case "PasswordRegisterEnabled":
